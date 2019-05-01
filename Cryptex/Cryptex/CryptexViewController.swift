21d115b6009fb4d7c65073c48cfb01775a31dc62
Christopher Aronson <christopheraronson@icloud.com>
2019-05-01 12:32:54 -0700

    Init commit and built basic interface.

---
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cryptex
+//
+//  Created by Christopher Aronson on 5/1/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var unlockButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unlockButtonPressed(_ sender: Any) {
+    }
+    
+}
+
