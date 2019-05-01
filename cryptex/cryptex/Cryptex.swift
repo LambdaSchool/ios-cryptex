8c1eb4a0d0a8f82a5d7ae035931b1399a7e46bff
Michael R.F <mrflowers22@uwalumni.com>
2019-05-01 14:59:32 -0400

    set up the storyboard. and assigned the scene to applicable source code files

---
+++ cryptex/cryptex/Cryptex.swift
@@ -0,0 +1,9 @@
+//
+//  Cryptex.swift
+//  cryptex
+//
+//  Created by Michael Flowers on 5/1/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
---
+++ cryptex/cryptex/CryptexViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CryptexViewController.swift
+//  cryptex
+//
+//  Created by Michael Flowers on 5/1/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var buttonProperties: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unlockButton(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
