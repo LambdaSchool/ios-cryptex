41ab0eaa3c67cdc463f4b7ff99421a1edabe365e
Jeffrey Carpenter <spaz926@me.com>
2019-05-01 14:36:08 -0500

    Added pickerview datasource methods

--- Cryptex/Cryptex/CryptexViewController.swift
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -14,23 +14,44 @@ class CryptexViewController: UIViewController {
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var unlockButton: UIButton!
     
+    var cryptexController = CryptexController()
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                   "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        updateViews()
     }
     
     @IBAction func unlockButtonPressed(_ sender: UIButton) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        
+        guard let currentCryptex = cryptexController.currentCryptex else { return }
+        
+        hintLabel.text = currentCryptex.hint
+        pickerView.reloadAllComponents()
     }
-    */
+}
 
+extension CryptexViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        guard let currentCryptex = cryptexController.currentCryptex else { return 0 }
+        
+        return currentCryptex.password.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return letters.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return letters[row]
+    }
 }
