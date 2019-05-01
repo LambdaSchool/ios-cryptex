4259a9443a1e938c6c371f2ab8df4b86b1b82c51
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-01 14:44:56 -0600

    Set up the initial view controller with some UI elements and created a Cryptex.swift file to configure the model object.

---
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CryptexViewController.swift
+//  Cryptex
+//
+//  Created by Mitchell Budge on 5/1/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var cryptexPickerView: UIPickerView!
+    @IBOutlet weak var cryptexButton: UIButton!
+    
+    @IBAction func cryptexButtonPressed(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
