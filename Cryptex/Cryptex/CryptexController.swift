c32871dab276f201d36da1b3b462d70de7704d0a
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-01 14:49:24 -0600

    Created a CryptexController.swift file to set up the initial array.

---
+++ Cryptex/Cryptex/CryptexController.swift
@@ -0,0 +1,14 @@
+//
+//  CryptexController.swift
+//  Cryptex
+//
+//  Created by Mitchell Budge on 5/1/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+class CryptexController {
+    private var cryptexArray: [Cryptex] = [B, L, U, E]
+    
+}
