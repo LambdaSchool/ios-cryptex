ac4c76bb3b5222aceea01c77616f89fd3b35e17a
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-01 18:14:45 -0400

    Through the first part of the game logic ...

--- Cryptex/Cryptex/CryptexViewController.swift
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -11,16 +11,16 @@ import UIKit
 class CryptexViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
   
-    let cryptext = CryptextController()
-    
-    @IBOutlet weak var cyrptexPickerView: UIPickerView!
+    let cryptextController = CryptextController()
+    // outlets
+    @IBOutlet weak var cryptextPickerView: UIPickerView!
     @IBOutlet weak var hintLabel: UILabel!
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.cyrptexPickerView.delegate = self
-        self.cyrptexPickerView.dataSource = self
+        self.cryptextPickerView.delegate = self
+        self.cryptextPickerView.dataSource = self
         updateViews()
     
     }
@@ -36,37 +36,53 @@ class CryptexViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+    // functs for view
     func updateViews() {
-        if let currentHint = cryptext.currentCryptext?.hint {
+        if let currentHint = cryptextController.currentCryptext?.hint {
             hintLabel.text = currentHint
     }
-        cyrptexPickerView.reloadAllComponents()
+        cryptextPickerView.reloadAllComponents()
     }
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        
-        
         // For the number of components, think about how you can figure out how many characters are in the `currentCryptex`'s password.
-        
-        return cryptext.currentCryptext!.password.count
+        return cryptextController.currentCryptext!.password.count
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         // For the number of rows, we want to show as many rows as there are letters.
-        
         return letters.count
     }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         // For the title of each row, we want to show the letter that corresponds to the row. i.e. row 0 should show "A", row 1 should show "B", etc.
-        
         return letters[row]
     }
-    
-    
-    
+    // game logic
+    private func hasMatchingPassword() -> Bool {
+        
+        // Get the title of each row (which should be a single letter) in the picker view (try using a for-in loop) and store each one in an array.
+        
+        guard let currentPassword = cryptextController.currentCryptext else { return false }
+        
+        var characters: [String] = []
+        
+        for i in 0 ... currentPassword.password.count {
+            
+            let row = cryptextPickerView.selectedRow(inComponent: i)
+            
+            
+            guard let title = pickerView(cryptextPickerView, titleForRow: row, forComponent: i) else { continue }
+                characters.append(title)
+        }
+
+
+        //combine array into a single string.
+        
+        let word = characters.joined().lowercased()
+        
+        //lower case and return bool
+        return word == currentPassword.password.lowercased()
+    }
     
     @IBAction func unlockButtonPressed(_ sender: Any) {
     }
+
 }
