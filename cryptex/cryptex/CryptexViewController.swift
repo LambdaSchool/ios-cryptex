543ab79f20819fa46ad2ac7c381b7a70cc20b938
Michael R.F <mrflowers22@uwalumni.com>
2019-05-01 15:35:12 -0400

    implemented the picker views delegate and data source methods.

--- cryptex/cryptex/CryptexViewController.swift
+++ cryptex/cryptex/CryptexViewController.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 class CryptexViewController: UIViewController {
-
+    
+    let cc = CryptexController()
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var buttonProperties: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        updateViews()
     }
     
     @IBAction func unlockButton(_ sender: UIButton) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews(){
+        label.text = cc.currentCryptex?.hint
+        pickerView.reloadAllComponents() //if we don't do this, when a new random cyrptex is started, the picker view won't update to show the (potential) new numbers of components
     }
-    */
+}
 
+//MARK: - PICKER VIEW DATA SOURCE AND DELEGATE
+extension CryptexViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //this sets how many columns or how wide the pickerview is, think width
+        return cc.currentCryptex?.password.count ?? 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //this sets up how many choices are in each column so think the height part of the pickerview
+        return letters.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let letter = letters[row]
+        return letter
+    }
 }
