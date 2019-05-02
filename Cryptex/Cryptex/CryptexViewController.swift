26db5b4193fc028ef076c3cac596d6f1af973daf
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-01 17:29:03 -0700

    CryptexViewController was not added before  this commit

---
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -0,0 +1,132 @@
+//
+//  CryptexViewController.swift
+//  Cryptex
+//
+//  Created by Hector Steven on 5/1/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+
+
+class CryptexViewController: UIViewController {
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		pickerView.delegate = self
+		pickerView.dataSource = self
+		updateViews()
+		reset()
+	}
+
+	func updateViews(){
+		if let cryptex = cryptexController.currentCryptex {
+			hintLabel?.text = cryptex.hint
+			pickerView.reloadAllComponents()
+		}
+	}
+	
+	@IBAction func unlockButtonPressed(_ sender: Any) {
+		print("unlock")
+		print(hasMatchingPassword())
+	}
+
+	var countdownTimer: Timer?
+	
+	@IBOutlet var hintLabel: UILabel!
+	@IBOutlet var pickerView: UIPickerView!
+	@IBOutlet var unlockButton: UIButton!
+
+	let cryptexController = CryptexController()
+	
+	var letters = ["A", "B", "C", "D",
+				   "E", "F", "G", "H",
+				   "I", "J", "K", "L",
+				   "M", "N", "O", "P",
+				   "Q", "R", "S", "T",
+				   "U", "V", "W", "X",
+				   "Y", "Z"]
+}
+
+extension CryptexViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+	
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		if let cryptex = cryptexController.currentCryptex {
+			return cryptex.password.count
+		}
+		
+		return 0
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return letters.count
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		for (index, char) in letters.enumerated() {
+			if index == row {
+				return String(char)
+			}
+		}
+		
+		return nil
+	}
+	
+}
+
+
+extension CryptexViewController {
+	func hasMatchingPassword() -> Bool {
+		let checkString = constructStringFromComponents()
+	
+		if let cryptex = cryptexController.currentCryptex {
+			if checkString.lowercased() == cryptex.password {
+				return true
+			}
+		}
+		
+		return false
+	}
+	
+	func constructStringFromComponents() -> String {
+		let numberOfComponents = pickerView.numberOfComponents - 1
+		var checkString = ""
+		
+		for index in 0...numberOfComponents {
+			let componentValue = pickerView.selectedRow(inComponent: index)
+			checkString += letters[componentValue]
+		}
+		
+		return checkString
+	}
+	
+	func reset() {
+		print("reset")
+		countdownTimer = nil
+		countdownTimer = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { timer in
+			print("Timer.invalidate")
+			timer.invalidate()
+			self.presentCorrectPasswordAlert()
+		}
+	}
+	
+	func newCryptexAndReset() {
+		cryptexController.randomCryptex()
+		updateViews()
+		reset()
+	}
+	
+	func presentCorrectPasswordAlert() {
+		guard let cryptex = cryptexController.currentCryptex else { return }
+		
+		let ac = UIAlertController(title: "Ran out of Time", message: "password is: \(cryptex.password)", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .cancel){ action in
+			self.newCryptexAndReset()
+		})
+		self.present(ac, animated: true)
+		
+	}
+}
