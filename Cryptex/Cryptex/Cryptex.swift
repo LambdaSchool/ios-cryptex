709df88cbb907ecd4d7096e0d20e57b7ebc655d8
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-08 15:38:10 -0400

    setup storyboard, cryptex model, and cryptex controller.

---
+++ Cryptex/Cryptex/Cryptex.swift
@@ -0,0 +1,14 @@
+//
+//  Cryptex.swift
+//  Cryptex
+//
+//  Created by Thomas Cacciatore on 5/8/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import Foundation
+
+struct Cryptex {
+    var password: String
+    var hint: String
+}
---
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CryptexViewController.swift
+//  Cryptex
+//
+//  Created by Thomas Cacciatore on 5/8/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
