d04882943669d11b1b568be5b121359e1bb289dd
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-08 11:09:26 -0600

    Set up the CryptexViewController.swift
    Added a number of functions per the instructions

--- Cryptex/Cryptex/CryptexViewController.swift
+++ Cryptex/Cryptex/CryptexViewController.swift
@@ -6,32 +6,141 @@
 //  Copyright © 2019 Mitchell Budge. All rights reserved.
 //
 
+
 import UIKit
 
-class CryptexViewController: UIViewController {
-
+class CryptexViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var cryptexController = CryptexController()
+    
+    @IBOutlet weak var cryptexPickerView: UIPickerView!
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var unlockButton: UIButton!
+    
+    
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cryptexPickerView.delegate = self
+        cryptexPickerView.dataSource = self
+        updateViews()
     }
-    @IBOutlet weak var hintLabel: UILabel!
-    @IBOutlet weak var cryptexPickerView: UIPickerView!
-    @IBOutlet weak var cryptexButton: UIButton!
     
-    @IBAction func cryptexButtonPressed(_ sender: Any) {
+    
+    func updateViews() {
+        guard let cryptex = cryptexController.currentCryptex else { return }
+        hintLabel.text = cryptex.hint
+        cryptexPickerView.reloadAllComponents()
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func unlockButtonPressed(_ sender: Any) {
+        if hasMatchingPassword() {
+            presentCorrectPasswordAlert()
+        } else {
+            presentIncorrectPasswordAlert()
+        }
     }
-    */
-
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        guard let currentCryptex = cryptexController.currentCryptex else { return 0}
+        return currentCryptex.password.count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 26
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return letters[row]
+    }
+    
+    
+    /*                              --Game Logic--                              */
+    
+    func hasMatchingPassword() -> Bool {
+        guard let currentPassword = cryptexController.currentCryptex else {return false}
+        var characters : [String] = []
+        
+        
+        for i in 0..<currentPassword.password.count{
+            let row = cryptexPickerView.selectedRow(inComponent: i)
+            guard let title = pickerView(cryptexPickerView, titleForRow: row, forComponent: i) else { continue }
+            characters.append(title)
+        }
+        
+        let answer = characters.joined()
+        if answer == cryptexController.currentCryptex?.password { return true } else { return false }
+        
+    }
+    
+    
+    var countdownTimer: Timer?
+    
+    
+    func reset() {
+        countdownTimer?.invalidate()
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 60, repeats: false, block: { (Timer) in
+            self.presentNoTimeRemainingAlert()
+        })
+        updateViews()
+    }
+    
+    func newCryptexAndReset() {
+        cryptexController.randomCryptex()
+        updateViews()
+        reset()
+    }
+    
+    
+    func presentCorrectPasswordAlert() {
+        
+        let correctAlert = UIAlertController(title: "CORRECT", message: "You answered correctly", preferredStyle: .alert)
+        correctAlert.addAction(UIAlertAction(title: "Next", style: .default, handler: {
+            (action) in
+            self.newCryptexAndReset()
+            
+        }))
+        present(correctAlert, animated: true, completion: nil)
+        
+    }
+    
+    func presentIncorrectPasswordAlert() {
+        
+        let incorrectAlert = UIAlertController(title: "WRONG", message: "Password entered is incorrect", preferredStyle: .alert)
+        incorrectAlert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: nil))
+        incorrectAlert.addAction(UIAlertAction(title: "Try New Cryptex", style: .default, handler: {
+            (action) in
+            self.newCryptexAndReset()
+        }))
+        present(incorrectAlert, animated: true, completion: nil)
+    }
+    
+    func presentNoTimeRemainingAlert() {
+        let noTimeAlert = UIAlertController(title: "OUT OF TIME", message: "You are out of time", preferredStyle: .alert)
+        noTimeAlert.addAction(UIAlertAction(title: "Reset timer?", style: .default, handler: {
+            (action) in
+            self.reset()
+        }))
+        noTimeAlert.addAction(UIAlertAction(title: "Try New Cryptex", style: .default, handler: {
+            (action) in
+            self.newCryptexAndReset()
+        }))
+        present(noTimeAlert, animated: true, completion: nil)
+    }
+    
+    
 }
