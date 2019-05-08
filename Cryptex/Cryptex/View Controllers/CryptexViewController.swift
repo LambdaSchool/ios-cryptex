8d8c162f028ae02fac58c5cff7e121073cfd9cf1
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-08 15:06:09 -0500

    Basic UI working

--- Cryptex/Cryptex/View Controllers/CryptexViewController.swift
+++ Cryptex/Cryptex/View Controllers/CryptexViewController.swift
@@ -9,27 +9,56 @@
 import UIKit
 
 class CryptexViewController: UIViewController {
+    
+    // MARK: - Properties
+    
     @IBOutlet weak var hintLabel: UILabel!
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var unlockButton: UIButton!
     
+    var cryptexController = CryptexController()
+    
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        updateViews()
     }
     
     @IBAction func unlockButtonTapped(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        hintLabel.text = cryptexController.currentCryptex?.hint
+        pickerView.reloadAllComponents()
     }
-    */
+}
 
+extension CryptexViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // For the number of components, think about how you can figure out how many characters are in the `currentCryptex`'s password.
+        guard let currentCryptex = cryptexController.currentCryptex else { return 0 }
+        return currentCryptex.password.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // For the number of rows, we want to show as many rows as there are letters.
+        return letters.count
+    }
+}
+
+extension CryptexViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // For the title of each row, we want to show the letter that corresponds to the row. i.e. row 0 should show "A", row 1 should show "B", etc.
+        return letters[row]
+    }
 }
