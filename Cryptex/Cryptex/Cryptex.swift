84338bca53104a1e8ed17e3efffa44dfd8d469d4
jayfenam <jonathanferrer95@gmail.com>
2019-05-08 16:31:27 -0400

    The app is fully functional Starting to add the go further features to it.

---
+++ Cryptex/Cryptex/Cryptex.swift
@@ -0,0 +1,16 @@
+//
+//  Cryptex.swift
+//  Cryptex
+//
+//  Created by Jonathan Ferrer on 5/8/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import Foundation
+
+struct Cryptex {
+
+    var password: String
+    var hint: String
+
+}
