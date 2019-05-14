76187c0cfacd571f6ae0525f1538358d8baf23cd
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-13 18:11:22 -0700

    set up storyboard

---
+++ Cryptex/ViewControllers/CryptexViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CryptexViewController.swift
+//  Cryptex
+//
+//  Created by Dongwoo Pae on 5/13/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    
+    @IBOutlet weak var hintLabel: UILabel!
+    
+    @IBOutlet weak var cryptexPickerView: UIPickerView!
+    
+    @IBOutlet weak var unlockButtonLabel: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    @IBAction func unlockButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
