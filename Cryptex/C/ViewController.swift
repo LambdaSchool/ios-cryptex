c425129f1f74a5fbc2964d7c53836f2ebbbf3982
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-14 21:23:50 -0400

    Iniital

---
+++ Cryptex/C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cryptex
+//
+//  Created by Nathan Hedgeman on 5/14/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
