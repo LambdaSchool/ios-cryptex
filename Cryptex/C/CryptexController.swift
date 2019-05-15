d879efb0a39a2afeee1e8f22adf00048f616b5f6
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-14 22:07:54 -0400

    added cryptex initializer with cryptexes and create function

--- Cryptex/C/CryptexController.swift
+++ Cryptex/C/CryptexController.swift
@@ -10,7 +10,40 @@ import Foundation
 
 class CryptexController {
     
-    private let cryptexes: [Cryptex] = []
+    init() {
+        
+        create(password: "Nathan", hint: "My Name")
+        create(password: "Lambda", hint: "My School")
+        create(password: "Mew", hint: "Best Pokemon")
+    }
+    
+    private (set) var cryptexes: [Cryptex] = []
+    
+    func create (password: String, hint: String) {
+        
+        let crypt = Cryptex(password: password, hint: hint)
+        
+        cryptexes.append(crypt)
+        
+    }
 
     
 }
+
+
+
+//class NoteController {
+//
+//    init () {
+//        //        createNote(withText: "Walk it")
+//        //        createNote(withText: "This is it")
+//        //        createNote(withText: "Last Call")
+//    }
+//    private(set) var notes: [Note] = []
+//
+//    func createNote(withText text: String) {
+//        let note = Note(text: text)
+//
+//        notes.append(note)
+//    }
+//}
